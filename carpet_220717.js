185d6dda4a711fcbf1403b3a9c9683251491c81b
AndrewKim330 <rendezvous330@gmail.com>
2022-07-17 14:15:48 +0900

    algo: Add carpet algorithm on Programmers

---
+++ carpet_220717.js
@@ -0,0 +1,25 @@
+// problems
+// 1. unpropriate consideration of case when number of yellow carpet is odd
+
+function solution(brown, yellow) {
+    let row, col;
+    //
+    let devideBy = 1;
+    // brute force dividing
+    while (yellow >= devideBy) {
+        // case when yellow carpet is devided by certain number
+        if (yellow % devideBy === 0) {
+            // variable 'devideBy' comes to number of line of carpet
+            const yellowDivision = yellow / devideBy;
+            const countBrown = 2 * denom + 2 * yellowDivision + 4;
+            // case when calculated number of brown carpet is equal to given number of brown carpet
+            if (countBrown === brown) {
+                row = denom + 2;
+                col = yellowDivision + 2;
+                break;
+            }
+        }
+        denom++;
+    }
+    return [col, row];
+}
